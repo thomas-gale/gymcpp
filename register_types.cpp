71e8d19e966564436948ff2b82e5da85e16aab78
Thomas Gale <gale.thomasjames@gmail.com>
2020-06-22 20:38:17 +0000

    Test module code builds and runs in godot.
    Moved the required registration code into the root of the repo.
    Next steps are to try and build a req and rep zmq implementation.
    (Where godot can block awaiting reset/step requests and return
    observation and reward signals).

--- register_types.cpp
+++ register_types.cpp
@@ -3,12 +3,12 @@
 #include "register_types.h"
 
 #include "core/class_db.h"
-#include "summator.h"
+#include "src/summator.h"
 
-void register_summator_types() {
+void register_gymcpp_types() {
     ClassDB::register_class<Summator>();
 }
 
-void unregister_summator_types() {
+void unregister_gymcpp_types() {
    // Nothing to do here in this example.
 }
