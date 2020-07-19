a342eea731f886d99d920b0ac5396e23ea743721
Thomas Gale <gale.thomasjames@gmail.com>
2020-07-19 17:02:32 +0100

    WIP: Experimenting with gRPC rather than ZMQ.

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 
+#include "zmq.hpp"
+
 namespace gym {
 
 Server::Server() {
@@ -14,7 +16,27 @@ Server::~Server() {
 
 void Server::serve() {
     // Blocking function serve forever till interrupted
-    // While true ....
+
+    // Create zmq context with single thread.
+    zmq::context_t context(1);
+
+    // Create responder
+    zmq::socket_t responder(context, ZMQ_REP);
+    responder.connect("tcp://localhost:5560");
+
+    while(true) {
+        //  Wait for next request from client
+        std::string string = responder.recv();
+        
+        std::cout << "Received request: " << string << std::endl;
+        
+        // Do some 'work'
+        sleep (1);
+        
+        //  Send reply back to client
+        s_send (responder, "World");
+    }
+
 }
 
 void Server::helloWorld() { std::cout << "Server hello world!\n"; }
--- test/server_test.cpp
+++ test/server_test.cpp
@@ -6,4 +6,20 @@ TEST(Server, CreateServer) {
     // Act
     gym::Server testServer;
 }
+
+TEST(Server, Serve) {
+    // Arrange
+    gym::Server testServer;
+
+    // Act
+    testServer.serve();
+}
+
+TEST(Server, HelloWorld) {
+    // Arrange
+    gym::Server testServer;
+
+    // Act
+    testServer.helloWorld();
+}
 } // namespace testgym
